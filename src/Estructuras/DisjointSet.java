118618628b934e9fdae6f5848c731bb0302b0045
tatlopez <tatianalopez0727@gmail.com>
2022-11-19 14:55:54 -0300

    operacion id en disjoint set

--- src/Estructuras/DisjointSet.java
+++ src/Estructuras/DisjointSet.java
@@ -11,5 +11,23 @@ public class DisjointSet {
             array[i] = -1;
         }
     }
+
+    public void Fusionar(int n, int m) {
+
+        
+        
+    }
+
+    public int Id(int n) {
+
+        if(array[n] > 0) { //si tengo un nro POSITIVO --> sigo buscando
+            n = array[n]; //ahora paso a buscar la raiz del que estaba en la posicion n
+            return array[n] = Id(n); //para optimizar, todos apuntan a la raiz, cuando vuelva a buscar van a ser menos llamadas
+        } else {
+            return n; //si tengo un nro negativo --> encontre el identificador
+        }
+
+        
+    }
     
 }
